bc32ef21889c8f0c011b08d3ec14ee0537d559d2
nmngn <51230684+ngaynam021@users.noreply.github.com>
2020-06-04 11:41:50 +0700

    Design UI, Apply MVC, Create Started View

---
+++ Spoon Master/Spoon Master/Controller/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Spoon Master
+//
+//  Created by Nam Ngây on 6/4/20.
+//  Copyright © 2020 Nam Ngây. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.hidesBackButton = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Spoon Master/Spoon Master/Controller/TipsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TipsViewController.swift
+//  Spoon Master
+//
+//  Created by Nam Ngây on 6/4/20.
+//  Copyright © 2020 Nam Ngây. All rights reserved.
+//
+
+import UIKit
+
+class TipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
